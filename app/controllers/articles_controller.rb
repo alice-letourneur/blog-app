d496a257e3fdc60b602a192f599277347506017a
“Alice <aliceletourneur@icloud.com>
2018-02-10 19:44:41 +0000

    add edit, update, show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,30 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
   end
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Arcticle was succesfullt created"
+      flash[:notice] = "Arcticle was succesfully created"
       redirect_to article_path(@article)
     else
       render 'new'
+      # or # render :new
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Arcticle was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
       # or # render :new
     end
   end
