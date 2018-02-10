47c5d91dd48c6f99f9501bc126721b1e48590a3b
“Alice <aliceletourneur@icloud.com>
2018-02-10 19:08:17 +0000

    fix create, add flash and error messages and create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Arcticle was succesfullt created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+      # or # render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+
   end
 
   private
